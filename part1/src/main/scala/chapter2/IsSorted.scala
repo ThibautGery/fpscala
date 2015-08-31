23fe594397b4fa92e58a5e7b98cd6fbd3faf255a
Thibaut Géry <thibaut.gery@gmail.com>
2015-08-31 17:48:45 +0200

    fix bug when the length of the list is strictly inferior to 2

--- part1/src/main/scala/chapter2/IsSorted.scala
+++ part1/src/main/scala/chapter2/IsSorted.scala
@@ -3,7 +3,8 @@ package chapter2
 object IsSorted {
 	def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
 		def loop(n : Int): Boolean = {
-			if(as.length == n + 2) ordered(as(n), as(n+1))
+			if(as.length < 2) true
+			else if(as.length == n + 2) ordered(as(n), as(n+1))
 			else if(!ordered(as(n), as(n+1))) false
 			else loop(n+1)
 		}
--- part1/src/test/scala/chapter2/IsSortedSpecs.scala
+++ part1/src/test/scala/chapter2/IsSortedSpecs.scala
@@ -3,6 +3,18 @@ package chapter2
 class IsSortedSpecs extends org.specs2.mutable.Specification {
 
 
+  "[]" >> {
+    "is sorted" >> {
+      IsSorted.isSorted(Array(),(a: Int, b: Int) => a < b ) must beTrue
+    }
+  }
+
+  "[0]" >> {
+    "is sorted" >> {
+      IsSorted.isSorted(Array(0),(a: Int, b: Int) => a < b ) must beTrue
+    }
+  }
+
   "[0, 1]" >> {
     "is sorted" >> {
       IsSorted.isSorted(Array(0, 1),(a: Int, b: Int) => a < b ) must beTrue
