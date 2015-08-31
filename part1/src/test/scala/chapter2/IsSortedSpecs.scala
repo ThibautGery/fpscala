d8004b3e6c2e632a17f9382c4865b9e254600045
Thibaut Géry <thibaut.gery@gmail.com>
2015-08-31 17:30:39 +0200

    add test on IsSorted

---
+++ part1/src/test/scala/chapter2/IsSortedSpecs.scala
@@ -0,0 +1,25 @@
+package chapter2
+
+class IsSortedSpecs extends org.specs2.mutable.Specification {
+
+
+  "[0, 1]" >> {
+    "is sorted" >> {
+      IsSorted.isSorted(Array(0, 1),(a: Int, b: Int) => a < b ) must beTrue
+    }
+  }
+
+
+  "[0, 1, 5, 8 ,9]" >> {
+    "is sorted" >> {
+      IsSorted.isSorted(Array(0, 1, 5, 8 ,9),(a: Int, b: Int) => a < b ) must beTrue
+    }
+  }
+
+
+  "[0, 1, 5, 8 ,9, 7]" >> {
+    "is not sorted" >> {
+      IsSorted.isSorted(Array(0, 1, 5, 8 ,9, 7),(a: Int, b: Int) => a < b ) must beFalse
+    }
+  }
+}
