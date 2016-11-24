b4b457364e02f15be47054ec1c9828d223416a6a
Thibaut Gery <thibaut.gery@gmail.com>
2016-11-24 16:41:01 +1100

    implement foldLeft as tail recursive function

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -59,10 +59,7 @@ object List {
     case Cons(a, t) => Cons(a, init(t))
   }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
-    case Nil => z
-    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-  }
+
   def sum2(ns: List[Int]) = foldRight(ns, 0)((x,y) => x + y)
   def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
 
@@ -72,6 +69,7 @@ object List {
   def length[A](as: List[A]): Int = foldRight(as, 0)((item, acc) => acc + 1)
 
   def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    @tailrec
     def loop(ass: List[A], acc: B): B = ass match {
       case Nil => acc
       case Cons(x, xs) => loop(xs, f(acc, x))
@@ -81,4 +79,5 @@ object List {
 
   def reverse[A](list: List[A]): List[A] = foldLeft[A, List[A]](list, Nil)((acc, item) =>  Cons(item, acc))
 
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = foldLeft(reverse(as), z)((b,a) => f(a,b))
 }
