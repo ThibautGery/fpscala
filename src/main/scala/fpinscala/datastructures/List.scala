9641588deec890059301f9311d1f34ccd757c90e
Thibaut Gery <thibaut.gery@gmail.com>
2016-11-24 16:05:49 +1100

    implement foldRight

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -58,4 +58,11 @@ object List {
     case Cons(a, Nil) => Nil
     case Cons(a, t) => Cons(a, init(t))
   }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x,y) => x + y)
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
 }
--- src/test/scala/fpinscala/datastructures/ListTest.scala
+++ src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -6,28 +6,34 @@ class ListTest extends Specification {
   "The sum" should {
     "return 0  for an empty list" in {
       List.sum(List()) must_== 0
+      List.sum2(List()) must_== 0
     }
 
     "return the value for list of one item" in {
       List.sum(List(12)) must_== 12
+      List.sum2(List(12)) must_== 12
     }
 
     "return the sum for list" in {
       List.sum(List(12,1)) must_== 13
+      List.sum2(List(12,1)) must_== 13
     }
   }
 
   "The product" in {
     "return 1 for an empty list" in {
       List.product(List()) must_== 1
+      List.product2(List()) must_== 1
     }
 
     "return 0 if one item is )" in {
       List.product(List(1, 45, 67,899,34, 0, 45)) must_== 0
+      List.product2(List(1, 45, 67,899,34, 0, 45)) must_== 0
     }
 
     "return the correct product of all the items" in {
       List.product(List(1, 2, 3, 5)) must_== 30
+      List.product2(List(1, 2, 3, 5)) must_== 30
     }
   }
 
