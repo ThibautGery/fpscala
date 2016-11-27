0be297bfb3e9f3e4ab224c0ee128e8b0ec3ab1de
Thibaut Gery <thibaut.gery@gmail.com>
2016-11-27 18:54:17 +1100

    implement max of tree

--- src/main/scala/fpinscala/datastructures/Tree.scala
+++ src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,6 +6,20 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  def max(t: Tree[Int]): Int = {
+    def max(t1: Tree[Int], t2: Tree[Int]): Int = (t1, t2) match {
+      case (Leaf(v1), Leaf(v2)) => v1.max(v2)
+      case (Branch(l, r), Leaf(v)) => max(l,r).max(v)
+      case (Leaf(v), Branch(l, r)) => max(l,r).max(v)
+      case (Branch(l1, r1), Branch(l2, r2)) => max(l1, r1).max(max(l2, r2))
+    }
+
+    t match {
+      case Leaf(v) => v
+      case Branch(t1, t2) => max(t1, t2)
+    }
+  }
+
   def size[A](t: Tree[A]) : Int = t match {
     case Branch(t1, t2) => size(t1) + size(t2) + 1
     case Leaf(_) => 1
--- src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -17,4 +17,20 @@ class TreeTest extends Specification {
       Tree.size(Branch(Branch(Leaf(1), Leaf(1)), Branch(Leaf(1), Leaf(1)))) must_== 7
     }
   }
+
+  "The maximun function" should {
+    "return the value for an leaf" in {
+      Tree.max(Leaf(1)) must_== 1
+    }
+
+    "return the max of the only two leaf" in {
+      Tree.max(Branch(Leaf(1), Leaf(2))) must_== 2
+    }
+
+    "return the max of the leaves" in {
+      Tree.max(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))) must_== 4
+      Tree.max(Branch(Branch(Leaf(1), Leaf(2)), Leaf(4))) must_== 4
+      Tree.max(Branch(Leaf(2), Branch(Leaf(3), Leaf(4)))) must_== 4
+    }
+  }
 }
