749116d5b6c17697b7165111ff2df594abad13ce
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-02 00:02:50 +1100

    implement variance

---
+++ src/main/scala/fpinscala/Math.scala
@@ -0,0 +1,21 @@
+package fpinscala
+
+/**
+  * Created by thibautgery on 1/12/16.
+  */
+object Math {
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs).flatMap { m =>
+      mean(xs.map(variancePerItem(m)))
+    }
+
+
+
+  def mean(xs: Seq[Double]): Option[Double] = {
+    if(xs.isEmpty) None
+    else Some(xs.sum / xs.size)
+  }
+
+  def variancePerItem(mean:Double)(item: Double): Double = math.pow(item - mean, 2)
+
+}
---
+++ src/test/scala/fpinscala/MathTest.scala
@@ -0,0 +1,35 @@
+package fpinscala
+
+import org.specs2.mutable.Specification
+
+
+class MathTest extends Specification {
+
+  "mean" should {
+    "return None for Nil" in {
+      Math.mean(Nil) must_== None
+    }
+
+    "return mean for integer list" in {
+      Math.mean(List(1, 2, 3)) must_== Some(2)
+    }
+  }
+
+  "variance per item" should {
+    "return the correct value " in {
+      Math.variancePerItem(2)(1) must_== 1
+      Math.variancePerItem(3)(1) must_== 4
+    }
+  }
+
+
+  "variance" should {
+    "return None for Nil" in {
+      Math.variance(Nil) must_== None
+    }
+
+    "return the computed variance" in {
+      Math.variance(List(600, 470, 170, 430, 300)) must_== Some(21704)
+    }
+  }
+}
