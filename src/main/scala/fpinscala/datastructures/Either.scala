89295daacb05ed6978e4795270d9e3dba374eeb8
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-03 10:19:49 +1100

    add either datatype

---
+++ src/main/scala/fpinscala/datastructures/Either.scala
@@ -0,0 +1,13 @@
+package fpinscala.datastructures
+
+
+sealed trait Either[+E, +A]
+case class Left[+E](value: E) extends Either[E, Nothing]
+case class Right[+A](value: A) extends Either[Nothing, A]
+
+
+object Either {
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e)}
+}
