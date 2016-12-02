200f12f1706e3175653ab77ec7eabba2e68372d7
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-03 10:34:33 +1100

    implement map & flatmap

--- src/main/scala/fpinscala/datastructures/Either.scala
+++ src/main/scala/fpinscala/datastructures/Either.scala
@@ -1,7 +1,17 @@
 package fpinscala.datastructures
 
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Left(e) => Left(e)
+    case Right(v) => Right(f(v))
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => Left(e)
+    case Right(v) => f(v)
+  }
+}
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
---
+++ src/test/scala/fpinscala/datastructures/EitherTest.scala
@@ -0,0 +1,30 @@
+package fpinscala.datastructures
+
+import org.specs2.mutable.Specification
+
+class EitherTest extends Specification {
+  "The map function" should {
+    "return Left for Left" in {
+      val either:Either[String, Int] = Left("toto")
+      either.map(i => i + 2) must_== Left("toto")
+    }
+
+    "return the value modified by the function" in {
+      val either:Either[String, Int] = Right(4)
+      either.map(i => i + 2) must_== Right(6)
+    }
+  }
+
+  "The flatmap function" should {
+    "return Left for Left" in {
+      val either:Either[String, Int] = Left("toto")
+      either.flatMap(i => Right(i + 2)) must_== Left("toto")
+    }
+
+    "return the value modified by the function" in {
+      val either: Either[String, Int] = Right(4)
+      either.flatMap(i => Right(i + 2)) must_== Right(6)
+      either.flatMap(i => Left("raté")) must_== Left("raté")
+    }
+  }
+}
