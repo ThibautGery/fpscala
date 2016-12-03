506340d855ce8ec1110a07742299e09fd7e3d94c
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-03 16:25:11 +1100

    implment take, takeWhile and drop

---
+++ src/main/scala/fpinscala/datastructures/nonStrict/Stream.scala
@@ -0,0 +1,48 @@
+package fpinscala.datastructures.nonStrict
+
+import scala.annotation.tailrec
+
+sealed trait Stream[+A] {
+  final def drop[B >: A](n: Int): Stream[B] = (this, n) match {
+    case (_, 0) => this
+    case (Empty, _) => Stream.empty
+    case (Cons(x, xs), i) => xs().drop( i - 1)
+  }
+
+  final def dropWhile[B >: A](f: B => Boolean): Stream[B] = this match {
+    case Cons(x, xs) if f(x()) => xs().dropWhile(f)
+    case _ => this
+  }
+
+  def take[B >: A](n: Int): Stream[B] = (this, n) match {
+    case (_, 0) => Stream.empty
+    case (Empty, _) => Stream.empty
+    case (Cons(x, xs), i) => Stream.cons(x(), xs().take(i - 1))
+  }
+
+  def toList[B >: A]: List[B] = {
+    @tailrec
+    def loop(s: Stream[B], acc: List[B]):List[B] = s match {
+      case Empty => acc
+      case Cons(x, xs) => loop(xs(), x() :: acc)
+    }
+    loop(this, Nil).reverse
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+
+object Stream {
+
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+}
---
+++ src/test/scala/fpinscala/datastructures/nonStrict/StreamTest.scala
@@ -0,0 +1,57 @@
+package fpinscala.datastructures.nonStrict
+
+import org.specs2.mutable.Specification
+
+/**
+  * Created by thibautgery on 3/12/16.
+  */
+class StreamTest extends Specification {
+
+  "toList function" should {
+    "convert the stream to list" in {
+      Stream(1, 2, 3, 4).toList must_== List(1, 2, 3, 4)
+    }
+  }
+
+  "drop function" should {
+    "return the empty stream if droping more elem than the list" in {
+      Stream(1, 2, 3, 4).drop(576) must_== Empty
+    }
+
+    "return the stream if dropping none" in {
+      Stream(1, 2, 3, 4).drop(0).toList must_== List(1, 2, 3, 4)
+    }
+
+    "return the stream without the first item" in {
+      Stream(1, 2, 3, 4).drop(2).toList must_== List( 3, 4)
+    }
+  }
+
+  "dropWhile function" should {
+    "return the empty stream if dropping more elem than the list" in {
+      Stream(1, 2, 3, 4).dropWhile(i => true) must_== Empty
+    }
+
+    "return the stream if dropping none" in {
+      Stream(1, 2, 3, 4).dropWhile(i => false).toList must_== List(1, 2, 3, 4)
+    }
+
+    "return the stream without the first item" in {
+      Stream(1, 2, 3, 4).dropWhile(i => i <= 2).toList must_== List( 3, 4)
+    }
+  }
+
+  "take function" should {
+    "return the empty stream if taking none" in {
+      Stream(1, 2, 3, 4).take(0) must_== Empty
+    }
+
+    "return the the stream if taking more" in {
+      Stream(1, 2, 3, 4).take(1000).toList must_== List(1, 2, 3, 4)
+    }
+
+    "return the stream without the last items" in {
+      Stream(1, 2, 3, 4).take(2).toList must_== List(1, 2)
+    }
+  }
+}
