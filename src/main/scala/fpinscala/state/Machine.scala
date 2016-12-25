a24697f2e39499ce69a83efcc3d1ea4b53623cfa
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-25 22:31:56 +1100

    setup machine class

---
+++ src/main/scala/fpinscala/state/Machine.scala
@@ -0,0 +1,11 @@
+package fpinscala.state
+
+sealed trait Input
+
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int) {
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+}
