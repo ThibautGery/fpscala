b671acf5bea6fc38d5120c5f8fbb1815b6a2c872
Thibaut Gery <thibaut.gery@gmail.com>
2016-12-27 00:31:07 +1100

    add Machine implementation

--- src/main/scala/fpinscala/state/Machine.scala
+++ src/main/scala/fpinscala/state/Machine.scala
@@ -7,5 +7,34 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int) {
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+  def processCoinInternal: ((Int, Int), Machine)= {
+    if(locked) Machine(locked = false, candies, coins + 1).toState
+    else this.toState
+  }
+
+  def processTurnInternal: ((Int, Int), Machine) = {
+    if(candies == 0 || locked) this.toState
+    else Machine(locked = true, candies - 1, coins).toState
+  }
+
+  private def toState = ((coins, candies), this)
+}
+
+object Machine {
+
+  type MachineSimulation = State[Machine, (Int, Int)]
+
+  def processCoin: MachineSimulation = State(_.processCoinInternal)
+  def processTurn: MachineSimulation = State(_.processTurnInternal)
+  def unit: MachineSimulation = State( machine => ((machine.coins, machine.candies), machine))
+
+  def simulateMachine(inputs: List[Input]): MachineSimulation =  {
+    State.sequence(unit :: inputs.map(simulateMachineForOne)).map(_.reverse.head)
+  }
+
+  def simulateMachineForOne(input: Input): MachineSimulation =  input match {
+    case Coin => processCoin
+    case Turn => processTurn
+  }
+
 }
---
+++ src/test/scala/fpinscala/state/MachineTest.scala
@@ -0,0 +1,47 @@
+package fpinscala.state
+
+import org.specs2.mutable.Specification
+
+
+class MachineTest extends Specification {
+  "process coin" should {
+    "do nothing if the machine is already unlock" in {
+      val machine = Machine(locked = false, 10, 0)
+      machine.processCoinInternal must_== ((0, 10), machine)
+    }
+
+    "unlock the machine and store one coin if it is lock" in {
+      val machine = Machine(locked = true, 10, 0)
+      machine.processCoinInternal must_== ((1, 10), Machine(locked = false, 10, 1))
+    }
+  }
+
+  "process turn" should {
+    "do nothing if the machine is lock" in {
+      val machine = Machine(locked = true, 10, 0)
+      machine.processTurnInternal must_== ((0, 10), machine)
+    }
+
+    "do nothing if they is no more candy" in {
+      val machine = Machine(locked = false, 0, 10)
+      machine.processTurnInternal must_== ((10, 0), machine)
+    }
+
+    "dispense a candy and lock the machine if it is unlock" in {
+      val machine = Machine(locked = false, 10, 1)
+      machine.processTurnInternal must_== ((1, 9), Machine(locked = true, 9, 1))
+    }
+  }
+
+  "simulateMachine" should {
+    "do nothing is no input" in {
+      val machine = Machine(locked = true, 5, 10)
+      Machine.simulateMachine(Nil).run(machine) must_== ((10, 5), machine)
+    }
+
+    "process input" in {
+      val machine = Machine(locked = true, 5, 10)
+      Machine.simulateMachine(List(Coin, Turn, Coin, Turn)).run(machine) must_== ((12, 3), Machine(locked = true, 3, 12))
+    }
+  }
+}
